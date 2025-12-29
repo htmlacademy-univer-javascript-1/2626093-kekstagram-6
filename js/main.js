5d93ac6aa725b206cac1ccac55ee2e338bd57776
UnusualNick <59228271+UnusualNick@users.noreply.github.com>
2025-12-29 10:35:02 +0500

    refactor: lots of misc changes, mostly best-practices implementation

--- js/main.js
+++ js/main.js
@@ -5,12 +5,6 @@ import { showAlert } from './utils.js';
 import { initFilters } from './filters.js';
 
 const initializeApp = () => {
-  if (typeof window.Pristine !== 'function') {
-    setTimeout(initializeApp, 100);
-    return;
-  }
-
-
   getData(
     (photos) => {
       renderThumbnails(photos);
--- js/utils.js
+++ js/utils.js
@@ -34,17 +34,7 @@ export const throttle = (callback, delayBetweenFrames) => {
 
 export const showAlert = (message) => {
   const alertContainer = document.createElement('div');
-  alertContainer.style.zIndex = '100';
-  alertContainer.style.position = 'absolute';
-  alertContainer.style.left = '0';
-  alertContainer.style.top = '0';
-  alertContainer.style.right = '0';
-  alertContainer.style.padding = '10px 3px';
-  alertContainer.style.fontSize = '30px';
-  alertContainer.style.textAlign = 'center';
-  alertContainer.style.backgroundColor = 'red';
   alertContainer.classList.add('data-error');
-
   alertContainer.textContent = message;
 
   document.body.append(alertContainer);
